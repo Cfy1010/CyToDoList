ecc13ab577b32af122980d26286a5db64cfffda9
Cyril <cy_one2000@yahoo.fr>
2020-01-12 16:30:13 +0100

    create count function

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 /**
  * Todolist
  */
+
 var app = {
+  counter: 0,
   init: function () {
     // je veux pouvoir acceder dans toute mon app à la div todo, donc je la stock dans une proprieté
     app.todo = document.getElementById('todo');
     // creer le formulaire pour creer une todo
     app.createForm();
     // creer le compteur
-    // app.createCount();
+    app.createCount();
     // // creer la liste des todos
     // app.createList();
   },
@@ -30,6 +32,22 @@ var app = {
     // ajouter mon formulaire dans la zone #todo
     app.todo.appendChild(form);
   },
+
+  createCount: function () {
+    // je crée une div
+    var counter = document.createElement('div');
+    // je mets un id à cette div
+    counter.id = 'todo-counter';
+    app.todo.appendChild(counter);
+    // je prend  la div et je la rends accessible dans app.counterDOM
+    app.counterDOM = counter;
+    app.updateCount();
+    app.todo.appendChild(counter);
+  },
+  updateCount: function () {
+    app.counterDOM.textContent = `${app.counter} taches(s) en cours`;
+  }
+
 };
 
 
