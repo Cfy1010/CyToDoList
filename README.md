ff4913a129c384befccf4c29d27a65adbc6cb6bc
Cyril <cy_one2000@yahoo.fr>
2020-01-12 16:00:33 +0100

    Display form done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-CyToDoList
+# CyToDoList
+1. Interface en JS
+
+2. Ajout d'une tâche en écrivant dans l'input puis en validant avec la touche Entrée.
+
+
+3. Check une tâche pour la marquer comme effectuée.
+
+4. Compteur de tâches non effectuées.
--- js/app.js
+++ js/app.js
@@ -2,24 +2,34 @@
  * Todolist
  */
 var app = {
-  init: function() {
+  init: function () {
+    // je veux pouvoir acceder dans toute mon app à la div todo, donc je la stock dans une proprieté
+    app.todo = document.getElementById('todo');
     // creer le formulaire pour creer une todo
     app.createForm();
     // creer le compteur
-    app.createCount();
-    // creer la liste des todos
-    app.createList();
+    // app.createCount();
+    // // creer la liste des todos
+    // app.createList();
   },
 
-  createForm: function ({
+  createForm: function () {
     // creer le formulaire
-
+    var form = document.createElement('form');
+    form.id = 'todo-form';
+    form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
     //  creer un input
-
+    var input = document.createElement('input');
+    input.type = 'text';
+    input.id = 'todo-input';
+    input.placeholder = 'Add task';
     // ajouter mon input dans le formulaire
-
+    form.appendChild(input);
     // ajouter mon formulaire dans la zone #todo
-  })
+    app.todo.appendChild(form);
+  },
 };
 
 
