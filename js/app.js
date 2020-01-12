3ab2592fb494eb99eeb503a08ab9eafdb60314f9
Cyril <cy_one2000@yahoo.fr>
2020-01-12 15:02:45 +0100

    Add structure & css

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/**
+ * Todolist
+ */
+var app = {
+  init: function() {
+    // creer le formulaire pour creer une todo
+    app.createForm();
+    // creer le compteur
+    app.createCount();
+    // creer la liste des todos
+    app.createList();
+  },
+
+  createForm: function ({
+    // creer le formulaire
+
+    //  creer un input
+
+    // ajouter mon input dans le formulaire
+
+    // ajouter mon formulaire dans la zone #todo
+  })
+};
+
+
+// Chargement du DOM
+document.addEventListener('DOMContentLoaded', app.init);
